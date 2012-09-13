e48e99c19fc300a15851966a6003230bfb4bacb1
Ben Youngblood <bx.youngblood@gmail.com>
2012-09-13 15:53:22 -0700

    now tracks upstream (or origin, if upstream is unavailable) remote

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ BjyModulus
 See the commit hash of the HEAD of all the loaded modules. Only supports git so far.
 
 Usage: install, enable, browse to /installed-modules
+
+Disclaimer
+----------
+This module is in no way related to the modulo (`%`) operator.
--- src/BjyModulus/Controller/ModulesController.php
+++ src/BjyModulus/Controller/ModulesController.php
@@ -18,14 +18,57 @@ class ModulesController extends AbstractActionController
 
             $pathArg = escapeshellarg($path);
 
-            $git = exec("cd $pathArg; git rev-parse HEAD");
-            if ($git == '') {
-                $git = 'module not tracked by git';
+            $localHash = exec("cd $pathArg; git rev-parse HEAD");
+            if ($localHash == '') {
+                $localHash = 'module not tracked by git';
             }
 
-            $moduleVersions[$name] = $git;
+            $remotes = exec("cd $pathArg; git remote");
+            $remotes = explode("\n", $remotes);
+
+            if (array_search("upstream", $remotes) !== false) {
+                $upstream = $this->getUpstreamHash($pathArg, 'upstream');
+            } else if (array_search("origin", $remotes) !== false) {
+                $upstream = $this->getUpstreamHash($pathArg, 'origin');
+            } else {
+                $upstream = 'n/a';
+            }
+
+            $moduleVersions[$name] = array(
+                'local'        => $localHash,
+                'upstreamHash' => $upstream['upstreamHash'],
+                'status'       => $upstream['status'],
+            );
         }
 
         return array('modules' => $moduleVersions);
+    }
+
+    protected function getUpstreamHash($pathArg, $remote)
+    {
+        exec("cd $pathArg; git fetch $remote");
+        $ahead = $behind = array();
+        exec("cd $pathArg; git rev-list $remote/master..HEAD", $ahead);
+        exec("cd $pathArg; git rev-list HEAD..$remote/master", $behind);
+
+        $ahead = count($ahead);
+        $behind = count($behind);
+
+        $hash = exec("cd $pathArg; git rev-parse $remote/master");
+
+        if ($ahead != 0 && $behind != 0) {
+            $status = "Your branch has diverged; $ahead ahead, $behind behind";
+        } else if ($ahead == 0 && $behind != 0) {
+            $status = "Your branch is $behind behind";
+        } else if ($ahead != 0 && $behind == 0) {
+            $status = "Your branch is $ahead ahead";
+        } else {
+            $status = "Up to date.";
+        }
+
+        return array(
+            'upstreamHash' => $hash,
+            'status'       => $status,
+        );
     }
 }
